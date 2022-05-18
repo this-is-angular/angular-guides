a2dafa67aa23beb694412c7266a323e2c2878670
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-05-18 02:03:38 +0200

    feat: add Functional programming section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,6 +128,12 @@ const config = {
             type: "doc",
           },
           {
+            docId: "/category/functional-programming",
+            label: "Functional programming",
+            position: "left",
+            type: "doc",
+          },
+          {
             docId: "/category/performance",
             label: "Performance",
             position: "left",
