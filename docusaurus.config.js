caba9b3ee152264f3f354a5e42dd7dcfe99354a4
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-05-28 00:23:10 +0200

    feat: add *Last updated* date and *Author* to content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,8 @@ const config = {
         },
         docs: {
           editUrl: `https://github.com/${organizationName}/${projectName}/edit/main`,
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
